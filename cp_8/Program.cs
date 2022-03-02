b4add99c4ed14b30ec439555582013840499e86b
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-02 23:35:45 +0530

    added core problems

---
+++ cp_8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cp_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int n;
+            Console.WriteLine("Enter a number to check : ");
+            n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine(n + " is an even number");
+            }
+            else
+            {
+                Console.WriteLine(n + " is an odd number");
+            }
+        }
+    }
+}
+
+  
+
