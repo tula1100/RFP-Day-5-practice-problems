7a42e48bfbbb21bcc2d74a069519105085b9a3e1
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-02 23:34:02 +0530

    added core problems

---
+++ cp_5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cp_5
+{
+    internal class Program
+    {
+        public static void primeFactors(int n)
+        {
+            while (n % 2 == 0)
+            {
+                Console.Write(2 + " ");
+                n /= 2;
+            }
+            for (int i = 3; i <= Math.Sqrt(n); i += 2)
+            { 
+                while (n % i == 0)
+                {
+                    Console.Write(i + " ");
+                    n /= i;
+                }
+            }
+            if (n > 2)
+                Console.Write(n);
+        }
+        public static void Main()
+        {
+            int n = 555;
+            primeFactors(n);
+        }
+
+    }
+}
+   
