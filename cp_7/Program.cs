86367f8883b5f3905e6cf2b7dfafee08e7ae75ee
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-02 23:35:11 +0530

    added core problems

---
+++ cp_7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cp_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20, b = 5;
+            Console.WriteLine("Before swap a= " + a + " b= " + b);
+            a = a * b;     
+            b = a / b;       
+            a = a / b;     
+            Console.WriteLine("After swap a= " + a + " b= " + b);
+        }
+    }
+}
+    
+
