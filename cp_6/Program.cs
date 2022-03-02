3fd45e2db9b72b39e052b270c976c22565eafd08
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-02 23:34:33 +0530

    added core problems

---
+++ cp_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cp_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring values
+            int dividend = 90, divisor = 11;
+            //Formula for quotient and remainder
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
+    
+
