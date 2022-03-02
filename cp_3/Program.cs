e04326a52fe3d218621bc2c1ab55b79447fbb37b
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-02 23:32:59 +0530

    added core problems

---
+++ cp_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cp_3
+{
+    internal class Program
+    {
+        public static int Powerof2(int n)
+        {
+            int result = 0;
+            for (int i = n; i >= 1; i--)
+            {
+                if ((i & (i - 1)) == 0)
+                {
+                    result = i;
+                    break;
+                }
+            }
+            return result;
+        }
+        static public void Main()
+        {
+            Console.WriteLine("enter the number");
+            int number = Convert.ToInt32((string)Console.ReadLine());
+            Powerof2(number);
+        }
+    }
+
+}
+    
+
