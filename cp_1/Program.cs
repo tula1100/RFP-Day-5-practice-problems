b67204bd24c761617a0c5c3de7d4f759ebbfe039
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-02 23:30:48 +0530

    added core problems

---
+++ cp_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cp_1
+{
+    internal class FlipCoinprogram
+    {
+        int headcount = 0;
+        int tailcount = 0;
+        public void FlipCoinMethod(int input)
+        {
+            int temp = input;
+            Random r = new Random();
+            while (input != 0)
+            {
+
+                double random = r.NextDouble();
+                if (random <= 0.5)
+                {
+                    tailcount++;
+                }
+                else
+                {
+                    headcount++;
+                }
+                input--;
+
+            }
+            Console.WriteLine((headcount * 101) / temp);
+            Console.WriteLine((tailcount * 101) / temp);
+
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            FlipCoinprogram f = new FlipCoinprogram();
+            f.FlipCoinMethod(number);
+        }
+
+    }
+}
