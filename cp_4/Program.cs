10d4f5cec59b433af908e8db7101a0b556bcb2d0
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-02 23:33:30 +0530

    added core problems

---
+++ cp_4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cp_4
+{
+    internal class Program
+    {
+        static double nthHarmonic(int n)
+        {
+            float harmonic = 1;
+
+            // loop to apply the formula
+            // Hn = H1 + H2 + H3 ... +
+            // Hn-1 + Hn-1 + 1/n
+            for (int i = 2; i <= n; i++)
+            {
+                harmonic += (float)1 / i;
+            }
+
+            return harmonic;
+        }
+        static public void Main()
+        {
+            int N = 10;
+
+            Console.Write(nthHarmonic(N));
+        }
+    }
+}
